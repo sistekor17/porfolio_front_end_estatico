24fb4a932f65328cf20b29852e1d842da49d8e48
sistekor17 <jamesserrato@hotmail.com>
2023-02-15 18:08:09 -0300

    Inicio de vinculacion de base de datos

--- Porfolioweb_Dinamico/src/app/services/porfolio.service.ts
+++ Porfolioweb_Dinamico/src/app/services/porfolio.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class PorfolioService {
+// Los servicios siempre se inyectan en el constructor
+  constructor(private http : HttpClient) { }
 
-  constructor() { }
-
-  obtenerDatos(){
-    console.log("El servicio Porfolio esta corriendo");
-  }
+  // obtenerDatos():observable <any>{
+  //   return this.http.get('json');
+  // }
 }
