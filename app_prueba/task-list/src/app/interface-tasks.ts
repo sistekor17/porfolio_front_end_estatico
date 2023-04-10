9a17e64b99988a4aa651fbf022b7299bef921d07
sistekor17 <jamesserrato@hotmail.com>
2023-04-10 10:17:52 -0300

    Se agrego el json de tareas

---
+++ app_prueba/task-list/src/app/interface-tasks.ts
@@ -0,0 +1,7 @@
+// En la interface task se define el tipo de los elementos que va a contener la lista
+export interface Task{
+    id?: number,
+    text: string,
+    day: string,
+    reminder: boolean
+}
---
+++ app_prueba/task-list/src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+//Al importarce la interfaz se le coloca el tipo a la lista TASKS
+import { Task } from "./interface-tasks"
+export const TASKS:Task[] = [
+    {
+        id: 1,
+        text : "Terminar la aplicacion Tasks de angular ",
+        day : "10 de abril",
+        reminder: true
+    },
+    {
+        id: 2,
+        text : "Terminar la api del backend ",
+        day : "11 de abril",
+        reminder: true
+    },
+    {
+        id: 1,
+        text : "Conectar el backend con el frontend ",
+        day : "12 de abril",
+        reminder: true
+    },
+    {
+        id: 1,
+        text : "Hacer la autenticacion del usuario ",
+        day : "13 de abril",
+        reminder: false
+    }
+]
